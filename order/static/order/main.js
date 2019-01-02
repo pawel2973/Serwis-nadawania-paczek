925142bbbf016f46b029fc61967f67efcb69597c
PatrykBrzuchacz <34620780+PatrykBrzuchacz@users.noreply.github.com>
2019-01-02 18:17:39 +0100

    Frontend - js autocomplete fields added

--- order/static/order/main.js
+++ order/static/order/main.js
@@ -29,4 +29,55 @@
         }
     });
 });
+
+$(document).ready(function(){
+    $("#id_type_2").on("click", function () {
+        if ($(this).is(":checked")) {
+				$(".waga").val(1).prop("readonly", false);
+            $(".dlugosc").val(9).prop("readonly", false);
+			$(".szerokosc").val(14).prop("readonly", false);
+			$(".wysokosc").val(7).prop("readonly", false);
+        } 
+		
+    });
+})
+$(document).ready(function(){
+    $("#id_type_0").on("click", function () {
+        if ($(this).is(":checked")) {
+			$(".waga").val(1).prop("readonly", true);
+            $(".dlugosc").val(35).prop("readonly", true);
+			$(".szerokosc").val(25).prop("readonly", true);
+			$(".wysokosc").val(5).prop("readonly", true);
+        } 
+    });
+});
+
+
+	$(document).ready(function(){
+    $("#id_type_1").on("click", function () {
+        if ($(this).is(":checked")) {
+				$(".waga").val(1000).prop("readonly", true);
+            $(".dlugosc").val(200).prop("readonly",false);
+			$(".szerokosc").val(140).prop("readonly",false);
+			$(".wysokosc").val(200).prop("readonly", true);
+        } 
+    });
+	});
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 })(jQuery);

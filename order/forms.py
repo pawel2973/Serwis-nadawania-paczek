8d945af1867968e9d4e694e0cbdac70520932733
alan-bialy <37514209+alan-bialy@users.noreply.github.com>
2019-01-03 04:49:00 +0100

    Fix ContactForm

--- order/forms.py
+++ order/forms.py
@@ -25,6 +25,7 @@ class AddressForm(forms.ModelForm):
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=30)
+    email = forms.EmailField()
     message = forms.CharField(
         max_length=2000,
         widget=forms.Textarea(),
@@ -34,6 +35,7 @@ class ContactForm(forms.Form):
     def clean(self):
         cleaned_data = super(ContactForm, self).clean()
         name = cleaned_data.get('name')
+        email = cleaned_data.get('email')
         message = cleaned_data.get('message')
         if not name and not email and not message:
             raise forms.ValidationError('You have to write something!')

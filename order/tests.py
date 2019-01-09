e9def32d3f6b12cc99a9ebffdcdd8a9201c9d7af
alan-bialy <37514209+alan-bialy@users.noreply.github.com>
2019-01-09 23:42:06 +0100

    Adding new tests (opinion)

--- order/tests.py
+++ order/tests.py
@@ -1,9 +1,14 @@
+from sqlite3 import IntegrityError
+
 from django.contrib.auth.models import User
+from django.db import transaction
+from django.shortcuts import redirect
 from django.test import TestCase
 from django.urls import reverse
 
 from .forms import FormParcelSize
-from .models import Courier, EnvelopePricing, Address, RecipientAddress, SenderAddress, PackPricing, PalletPricing
+from .models import Courier, EnvelopePricing, Address, RecipientAddress, SenderAddress, PackPricing, PalletPricing, \
+    Parcel, Order, Profile, Opinion
 
 
 # Test for models.py
@@ -22,6 +27,24 @@ class ModelTest(TestCase):
                         'street': 'Kwiatowa', 'house_number': 52, 'telephone_number': '888000111',
                         'email_address': 'correct@email.ad'}
         self.address_object = Address.objects.create(**data_address)
+
+        RecipientAddress.objects.create(address=self.address_object)
+        SenderAddress.objects.create(address=self.address_object)
+
+        Parcel.objects.create(length=4, width=4, height=5, weight=2, type='Koperta')
+        Parcel.objects.create(length=4, width=4, height=5, weight=2, type='Koperta')
+
+        User.objects.create_user(username='Tester', password='for_test')
+        User.objects.create_user(username='Tester2', password='for_test')
+
+        Order.objects.create(profile=Profile.objects.get(pk=1), courier=Courier.objects.get(pk=1),
+                             parcel=Parcel.objects.get(pk=1), recipient=RecipientAddress.objects.get(pk=1),
+                             sender=SenderAddress.objects.get(pk=1), price=22.5)
+        Order.objects.create(profile=Profile.objects.get(pk=2), courier=Courier.objects.get(pk=1),
+                             parcel=Parcel.objects.get(pk=2), recipient=RecipientAddress.objects.get(pk=1),
+                             sender=SenderAddress.objects.get(pk=1), price=22.5)
+
+        Opinion.objects.create(order=Order.objects.get(pk=1), content='Wszystko poszlo sprawnie 10/10', rating=10)
 
     def test_courier_content(self):
         expected_object_name = '{}'.format(self.courier_object.name)
@@ -62,13 +85,13 @@ class ModelTest(TestCase):
                              self.address_object.email_address) + " | " + str(show_company) + str(show_nip))
 
     def test_address_rec_self(self):
-        address_recipient = RecipientAddress.objects.create(address=self.address_object)
+        address_recipient = RecipientAddress.objects.get(pk=1)
         self.assertTrue(isinstance(address_recipient, RecipientAddress))
         self.assertEqual(address_recipient.__str__(),
                          address_recipient.address.name + " " + address_recipient.address.surname)
 
     def test_address_sender_self(self):
-        address_sender = SenderAddress.objects.create(address=self.address_object)
+        address_sender = SenderAddress.objects.get(pk=1)
         self.assertTrue(isinstance(address_sender, SenderAddress))
         self.assertEqual(address_sender.__str__(),
                          address_sender.address.name + " " + address_sender.address.surname)
@@ -86,6 +109,80 @@ class ModelTest(TestCase):
         self.assertEqual(pack.__str__(), pack.courier.__str__())
         self.assertEqual(pallet.__str__(), pallet.courier.__str__())
 
+    def test_parcel_self(self):
+        parcel = Parcel.objects.get(pk=1)
+        self.assertTrue(isinstance(parcel, Parcel))
+        self.assertEqual(parcel.__str__(),
+                         parcel.type + " / " + str(parcel.weight) + " kg / " + str(parcel.length) + " x " + str(
+                             parcel.weight) + " x " + str(parcel.height) + " cm ")
+
+    def test_profile_self(self):
+        profile = Profile.objects.get(pk=1)
+        self.assertTrue(isinstance(profile, Profile))
+        self.assertEqual(profile.__str__(), profile.user.__str__())
+
+    def test_order_self(self):
+        PACK_STATUS = (
+            (0, 'Przygotowane do wysyłki'),
+            (1, 'W drodze'),
+            (2, 'Dostarczono'),
+            (3, 'Anulowano'),
+        )
+        order = Order.objects.get(pk=1)
+        self.assertTrue(isinstance(order, Order))
+        self.assertEqual(order.__str__(), "#" + str(order.id) + " | " + str(order.courier) + " | " + str(
+            order.parcel) + " | status: " + str(PACK_STATUS[order.status][1]) + " | cena: " + str(order.price) + " zł")
+
+    def test_opinion_self(self):
+        opinion = Opinion.objects.get(pk=1)
+        self.assertTrue(isinstance(opinion, Opinion))
+        self.assertEqual(opinion.__str__(),
+                         "Ocena: " + str(opinion.rating) + " | " + str(opinion.order.courier) + "  | Dnia: " + str(
+                             opinion.date))
+
+    def test_opinion_view(self):
+        url = reverse('order:opinion_create')
+        self.client.login(username='Tester', password='for_test')
+
+        # without entering order_id
+        response = self.client.get(url)
+        self.assertEqual(302, response.status_code)
+
+        # entering opinion with order_id
+        order_id = Order.objects.get(pk=1).id
+        response = self.client.get(url, {'order_id': order_id})
+        self.assertEqual(200, response.status_code)
+
+        # entering opinion with other user order_id
+        order_id = Order.objects.get(pk=2).id
+        response = self.client.get(url, {'order_id': order_id})
+        self.assertEqual(302, response.status_code)
+
+        self.client.logout()
+        self.client.login(username='Tester2', password='for_test')
+
+        # entering without post data
+        response = self.client.post('/courier-opinion-create/?order_id=2', {})
+        self.assertEqual(response.status_code, 200)
+        exp_data = {
+            'rating': ['To pole jest wymagane.'],
+            'content': ['To pole jest wymagane.'],
+        }
+        self.assertEqual(exp_data, response.context['form'].errors)
+
+        # entering with post data
+        req_data = {
+            'content': 'Super szybka przesylka!',
+            'rating': 10,
+        }
+        response = self.client.post('/courier-opinion-create/?order_id=2', req_data)
+        self.assertRedirects(response,
+                             reverse('order:pricing_company', kwargs={'pk': Order.objects.get(pk=order_id).courier_id}))
+        # creating when there is opinino for this order
+        with transaction.atomic():
+            response = self.client.post('/courier-opinion-create/?order_id=2', req_data)
+        self.assertEqual('To zamowienie juz ma dodana opinie',response.context['error_unique'])
+
 
 # tests for forms.py
 class FormsTest(TestCase):
@@ -98,6 +195,7 @@ class FormsTest(TestCase):
         data = {'type': 'koperta', 'weight': -1, 'length': 1, 'width': 1, 'height': 1}
         form = FormParcelSize(data=data)
         self.assertFalse(form.is_valid())
+
 
 class UrlsTest(TestCase):
     def setUp(self):
@@ -190,15 +288,22 @@ class UrlsTest(TestCase):
         response = self.client.get(reverse('order:orders'))
         self.assertEqual(302, response.status_code)
 
-    #checking ranking without couriers in database
+    # checking ranking without couriers in database
     def test_ranking_view(self):
         response = self.client.get(reverse('order:ranking'))
         self.assertEqual(200, response.status_code)
 
+    # checking creating opinion without loggin in
+    def test_create_opinion_view(self):
+        response = self.client.get(reverse('order:opinion_create'))
+        self.assertEqual(302, response.status_code)
+
+    # checking creating opinion logged in without having order-id
+    def test_create_opinion_logged_in(self):
+        self.client.login(username='Tester', password='for_test')
+        response = self.client.get(reverse('order:opinion_create'))
+        self.assertEqual(302, response.status_code)
+
     def test_made_up_page(self):
         response = self.client.get('/order/made_up_url.html')
         self.assertEqual(404, response.status_code)
-
-# class IndexViewTest(TestCase):
-#     def choose_pricing_test(self):
-#         response = self.client.get(reverse('order:index'))
